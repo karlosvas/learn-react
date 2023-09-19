a72b4c0ec3db37b124263fe35dedebf34fcfa980
karlosvas <126987511+karlosvas@users.noreply.github.com>
2023-09-19 12:06:00 +0200

    React class started

---
+++ projects/react-project/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+export function App () {
+    return (
+    <article>
+        <header>
+            <img alt="El avatar de karlosvas" src="https://avatars.githubusercontent.com/u/126987511?s=400&u=e8f57c914e30645db5bc77f1763440be810e3a91&v=4" />
+            <div>
+                <strong>karlosvas</strong>
+            </div>
+        </header>
+
+        <aside>
+            <button>
+                Seguir
+            </button>
+        </aside>
+    </article>
+    )
+}
